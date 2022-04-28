6e98ada937fd605454efcb8c08739cb0fac694ea
Victor Procure <victor.procure@gmail.com>
2022-04-28 12:00:02 -0600

    feat: Initial Commit

---
+++ DraggableReorder/ItemsProviderRequest.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading;
+
+namespace DraggableReorder
+{
+    public struct ItemsProviderRequest
+    {
+        public CancellationToken CancellationToken { get; }
+
+        public ItemsProviderRequest(CancellationToken cancellationToken) => CancellationToken = cancellationToken;
+    }
+}
---
+++ DraggableReorder/ItemsProviderResult.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace DraggableReorder
+{
+#if !NET5_0_OR_GREATER
+    public struct ItemsProviderResult<TItem>
+    {
+        public IEnumerable<TItem> Items { get; }
+
+        public int TotalItemCount { get; }
+
+        public ItemsProviderResult(IEnumerable<TItem> items, int totalItemCount)
+        {
+            Items = items;
+            TotalItemCount = totalItemCount;
+        }
+    }
+#endif
+}
